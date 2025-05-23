d213137a5d051775235c236b7c9ef5d7d785c513
Medoed <stark49@mail.ru>
2025-05-23 18:02:53 +0300

    async parser Spimex results

---
+++ part_4/task/database.py
@@ -0,0 +1,39 @@
+import asyncio
+import datetime
+from sqlalchemy import String, Integer, DateTime, func
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL, pool_pre_ping=True)
+Session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class SpimexTradingResult(Base):
+    __tablename__ = "spimex_trading_results"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    exchange_product_id: Mapped[str] = mapped_column(String(20))
+    exchange_product_name: Mapped[str] = mapped_column(String(200))
+    oil_id: Mapped[str] = mapped_column(String(4))
+    delivery_basis_id: Mapped[str] = mapped_column(String(3))
+    delivery_basis_name: Mapped[str] = mapped_column(String(50))
+    delivery_type_id: Mapped[str] = mapped_column(String(1))
+    volume: Mapped[int] = mapped_column(Integer)
+    total: Mapped[int] = mapped_column(Integer)
+    count: Mapped[int] = mapped_column(Integer)
+    date: Mapped[str] = mapped_column(String(10))
+    created_on: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_on: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+
+
+async def create_all():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        print("Таблица создана")
---
+++ part_4/task/service.py
@@ -0,0 +1,79 @@
+import os
+import uuid
+import pandas as pd
+import aiohttp
+import aiofiles
+
+from database import Session, SpimexTradingResult
+
+
+async def get_content_from_link(href):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(href, allow_redirects=True) as response:
+                if response.status != 200:
+                    print(f'Не  удалось получить информацию по ссылке {href}')
+                    return None
+                data = await response.read()
+                return data
+    except Exception as e:
+        print(f"Не удалось получить информацию по ссылке {href}. Ошибка: {str(e)}")
+        return None
+
+
+async def save_content_to_xls(content):
+    file_path = f'files/{str(uuid.uuid4())}.xls'
+    async with aiofiles.open(file_path, 'wb+') as file:
+        await file.write(content)
+    return file_path
+
+
+async def save_data_from_link(href):
+    content = await get_content_from_link(href)
+    if not content:
+        return 0
+    xls_path = await save_content_to_xls(content)
+
+    try:
+        async with Session() as session:
+            counter = 0
+            df = pd.read_excel(xls_path)
+            date = df['Форма СЭТ-БТ'][2].replace('Дата торгов: ', '')
+            metric_ton_rows = df[df.eq("Единица измерения: Метрическая тонна").any(axis=1)].index.tolist()
+            if not metric_ton_rows:
+                return 0
+            row_with_metric_ton = int(metric_ton_rows[0])
+            df = df[row_with_metric_ton + 3:]
+
+            total_row = int(df[df.eq("Итого:").any(axis=1)].index.tolist()[0])
+            df = df[:total_row - row_with_metric_ton - 3]
+            df = df[df['Unnamed: 14'] != '-']
+            for index, row in df.iterrows():
+                try:
+                    exchange_product_id = row['Форма СЭТ-БТ']
+                    if len(exchange_product_id) == 11:
+                        new_trading_result = SpimexTradingResult(
+                            id=str(uuid.uuid4()),
+                            exchange_product_id=exchange_product_id,
+                            exchange_product_name=row['Unnamed: 2'],
+                            oil_id=exchange_product_id[:4],
+                            delivery_basis_id=exchange_product_id[4:7],
+                            delivery_basis_name=row['Unnamed: 3'],
+                            delivery_type_id=exchange_product_id[-1],
+                            volume=int(row['Unnamed: 4']),
+                            total=int(row['Unnamed: 5']),
+                            count=int(row['Unnamed: 14']),
+                            date=date,
+                        )
+                        session.add(new_trading_result)
+                        counter += 1
+                except Exception as e:
+                    print(e)
+
+            await session.commit()
+        print(f"Сохранено {counter} записей из ссылки {href}.")
+    except Exception as e:
+        print(e)
+    finally:
+        os.remove(xls_path)
+        return counter
